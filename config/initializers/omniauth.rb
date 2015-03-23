57afd93b1ed5586ec63211ea00aaabab7e0b9e77
Sean Fenton <sfenton@weblinc.com>
2015-03-23 09:57:19 -0400

    FBLOGIN-1: Add config option to omniauth builder

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Weblinc.config.facebook_key, Weblinc.config.facebook_secret
+  provider :facebook, Weblinc.config.facebook_key, Weblinc.config.facebook_secret, Weblinc.config.facebook_options || {}
 end
--- lib/weblinc/facebook/version.rb
+++ lib/weblinc/facebook/version.rb
@@ -1,5 +1,5 @@
 module Weblinc
   module Facebook
-    VERSION = "0.0.4"
+    VERSION = "0.0.5"
   end
 end
