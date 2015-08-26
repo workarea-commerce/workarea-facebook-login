1217af08fda2c5437116d8c7b689e8b90c5895ac
Zachary Owen <zowen@weblinc.com>
2015-08-26 15:59:26 -0400

    FBLOGIN-6: adds default facebook options to ensure email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Weblinc.config.facebook_key, Weblinc.config.facebook_secret, Weblinc.config.facebook_options || {}
+  provider :facebook,
+    Weblinc.config.facebook_key,
+    Weblinc.config.facebook_secret,
+    Weblinc.config.facebook_options || {
+      scope: 'public_profile,email', info_fields: 'email,name'
+    }
 end
