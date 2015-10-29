5ca8d77e9e066f860e81393732d44b9f81b472fc
Hung Tran <hung@revelry.co>
2015-10-29 13:18:20 -0500

    FB-766: set omniauth options to use API 2.5 and parse token response as JSON

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Weblinc.config.facebook_key,
     Weblinc.config.facebook_secret,
     Weblinc.config.facebook_options || {
-      scope: 'public_profile,email', info_fields: 'email,name'
+      scope: 'public_profile,email',
+      info_fields: 'email,name',
+      client_options: {
+        site: 'https://graph.facebook.com/v2.5',
+        authorize_url: 'https://www.facebook.com/v2.5/dialog/oauth'
+      },
+      token_params: {
+        parse: :json
+      }
     }
 end
